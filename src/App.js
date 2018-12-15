ff65839083879533229d117593dbf2f838fe8e8b
MattSaeeda <xzarbx@gmail.com>
2018-12-15 16:08:27 -0500

    changed post variables to car

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Menu from './components/MenuComponent';
 import NavBar from './components/NavBarComponent';
 import Footer from './components/FooterComponent';
 import './App.css';
-import {POSTS} from './shared/posts';
+import {cars} from './shared/cars';
 
 class App extends Component {
 
@@ -13,7 +13,10 @@ class App extends Component {
     super(props);
 
     this.state = {
-      posts: POSTS
+      cars: cars,
+      // web3: null,
+      // accounts: null,
+      // contract: null
     };
   }
   render() {
@@ -25,11 +28,37 @@ class App extends Component {
         //     <h1>My Blog App</h1><button>Log in</button><button>Logout</button>
         //   </div>
         // </Navbar> */ }
-        <Menu posts={this.state.posts}/>
+        <Menu cars={this.state.cars}/>
          <Footer /> 
       </div>
     );
   }
 }
+
+
+// componentDidMount = async () => {
+//   try {
+//     // Get network provider and web3 instance.
+//     const web3 = await getWeb3();
+
+//     // Use web3 to get the user's accounts.
+//     const accounts = await web3.eth.getAccounts();
+
+//     // Get the contract instance.
+//     const Contract = truffleContract(Rent);
+//     Contract.setProvider(web3.currentProvider);
+//     const instance = await Contract.deployed();
+
+//     // Set web3, accounts, and contract to the state
+//     this.setState({ web3, accounts, contract: instance });
+//   } catch (error) {
+//     // Catch any errors for any of the above operations.
+//     alert(
+//       `Failed to load web3, accounts, or contract. Check console for details.`
+//     );
+//     console.log(error);
+//   }
+// };
+
 
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle, Button, InputGroupAddon, InputGroup,
      InputGroupText, Input} from 'reactstrap';
+//import getWeb3 from "./utils/getWeb3";
+import {cars} from '../shared/cars';
 
 class Menu extends Component {
  
@@ -8,79 +10,151 @@ class Menu extends Component {
         super(props);
 
         this.state = {
-            selectedPost: null
-        }
+            selectedCar: null,
+            rentPeriod: 0,
+            estimatedCost: 0
+        };
+
+        this.handleChange = this.handleChange.bind(this);
     }  
         
-        onPostSelect(post) {
-            this.setState({selectedPost: post});
-            console.log("button clicked, post selected.")
+    handleChange(event) {
+            const newRentPeriod = event.target.value;
+            console.log(newRentPeriod)
+            this.setState({
+                rentPeriod : newRentPeriod,
+                estimatedCost:  newRentPeriod*cars.pricePerDay
+            })
         }
 
-        renderPost(post) {
-            if (post != null) {
-                console.log("render started to show post")
-                return(
-                    <Card>
-                        <CardImg  top width="100%" src={post.picture}/>
-                        <CardBody>
-                            <CardTitle>{post.name}</CardTitle>
-                            <CardText>{post.pricePerDay}</CardText>
-                            <Button>Edit</Button>
-                        </CardBody>
-                    </Card>
-                );
-            }
-            else{
-                return(
-                    <div></div>
-                );
-            }
-        }
+render() {
+
+    const menu = this.props.cars.map((car) => {
+        return (
+            <div key={car.id} className = "shadow p-3 col-12 col-md-4">
+                <Card >
+                    <CardImg  top width="100%" src={car.picture}/>
+                    <CardBody>
+                        <CardTitle>{car.name}</CardTitle>
+                        <CardText>{car.pricePerDay} Ethers Per day</CardText>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">Please enter the rent period: </InputGroupAddon>
+                            <Input placeholder="Days" type="number" step="1" onChange={this.handleChange} />                          
+                        </InputGroup>
+                        <br />
+                        <cardText>Estimated total cost: {this.state.estimatedCost}</cardText>
+                        <br/>
+                        <CardText>You'll earn {car.CRTTokenPerDay*this.state.rentPeriod} CRT Tokens</CardText>
+                        <Button color="primary">Rent</Button>
+                        <Button color="danger">Return</Button>
+                        <Button color="success">Show my CRT balance</Button>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    });
+    return (
+        <div className="container-fluid" color>
+            <div className="row">
+                {menu}
+            </div>
+            
+        </div>
+    )
+}
+}
+        
+        // handleChange(event) {
+        //     const newRentPeriod = event.target.value;
+        //     this.setState({
+        //         rentPeriod : newRentPeriod,
+        //         estimatedCost:  newRentPeriod*CARS.pricePerDay})
+        // }
+
+        // handleRent(event) {
+        //     componentDidMount = async () => {
+        //         try {
+        //           // Get network provider and web3 instance.
+        //           const web3 = await getWeb3();
+            
+        //           // Use web3 to get the user's accounts.
+        //           const accounts = await web3.eth.getAccounts();
+            
+        //           // Get the contract instance.
+        //           const Contract = truffleContract(Rent);
+        //           Contract.setProvider(web3.currentProvider);
+        //           const instance = await Contract.deployed();
+            
+        //           // Set web3, accounts, and contract to the state, and then proceed with an
+        //           // example of interacting with the contract's methods.
+        //           this.setState({ web3, accounts, contract: instance }, this.runExample(event));
+        //         } catch (error) {
+        //           // Catch any errors for any of the above operations.
+        //           alert(
+        //             `Failed to load web3, accounts, or contract. Check console for details.`
+        //           );
+        //           console.log(error);
+        //         }
+        //       };
+
+        // }
+
+        // runExample = async (_car) => {
+        //     const { accounts, contract } = this.state;
+        
+        //     await contract.rent(CARS.id);
+        //     await contract.transfer(this.account, account[0])
+        
+        //     // Get the value from the contract to prove it worked.
+        //     const response = await contract.get();
+        
+        //     // Update state with the result.
+        //     this.setState({ storageValue: response.toNumber() });
+        //   };
     
 
-    render() {
+    // render() {
 
-        const menu = this.props.posts.map((post) => {
-            return (
-                <div key={post.id} className = "shadow p-3 col-12 col-md-4">
-                    <Card >
-                        <CardImg  top width="100%" src={post.picture}/>
-                        <CardBody>
-                            <CardTitle>{post.name}</CardTitle>
-                            <CardText>{post.pricePerDay} Ether Per day</CardText>
-                            <InputGroup>
-                                <InputGroupAddon addonType="prepend">Please enter the rent period: </InputGroupAddon>
-                                <Input placeholder="Days" type="number" step="1" />                          
-                            </InputGroup>
-                            <br />
-                            <InputGroup>
-                                <InputGroupAddon addonType="prepend">Estimated total cost: </InputGroupAddon>
-                                <Input placeholder= {post.name} type="number"  />
-                                <InputGroupAddon addonType="append">Ethers</InputGroupAddon> 
-                            </InputGroup>
-                            <br/>
-                            <CardText>You'll earn {post.name} CRT Tokens</CardText>
-                            <Button color="primary" onClick={() => this.onPostSelect(post)}>Rent</Button>
-                            <Button color="danger">Return</Button>
-                            <Button color="success">Show my CRT balance</Button>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
-        });
+    //     const menu = this.props.cars.map((car) => {
+    //         return (
+    //             <div key={car.id} className = "shadow p-3 col-12 col-md-4">
+    //                 <Card >
+    //                     <CardImg  top width="100%" src={car.picture}/>
+    //                     <CardBody>
+    //                         <CardTitle>{car.name}</CardTitle>
+    //                         <CardText>{car.pricePerDay} Ether Per day</CardText>
+    //                         <InputGroup>
+    //                             <InputGroupAddon addonType="prepend">Please enter the rent period: </InputGroupAddon>
+    //                             <Input placeholder="Days" type="number" step="1" />                          
+    //                         </InputGroup>
+    //                         <br />
+    //                         <InputGroup>
+    //                             <InputGroupAddon addonType="prepend">Estimated total cost: </InputGroupAddon>
+    //                             <Input placeholder= {this.state.estimatedCost} type="number"  />
+    //                             <InputGroupAddon addonType="append">Ethers</InputGroupAddon> 
+    //                         </InputGroup>
+    //                         <br/>
+    //                         <CardText>You'll earn {car.CRTTokenPerDay*this.rentPeriod} CRT Tokens</CardText> */}
+    //                         {/* <Button color="primary" onClick={() => this.handleRent(car)}>Rent</Button> */}
+    //                         <Button color="danger">Return</Button>
+    //                         <Button color="success">Show my CRT balance</Button>
+    //                     </CardBody>
+    //                 </Card>
+    //             </div>
+    //         );
+    //     });
 
-        return (
-            <div className="container-fluid" color>
-                <div className="row">
-                    {menu}
-                </div>
-                <div className="col">
-                    {this.renderPost(this.state.selectedPost)}
-                </div>
-            </div>
-        )
-    }
-}
+    //     return (
+    //         <div className="container-fluid" color>
+    //             <div className="row">
+    //                 {menu}
+    //             </div>
+    //             <div className="col">
+    //                 {this.renderPost(this.state.selectedPost)}
+    //             </div>
+    //         </div>
+    //     )
+    // }
+
 
 export default Menu
